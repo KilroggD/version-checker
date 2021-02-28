f272d9fbde2869a6adcf485b2653a14144b80310
Vladimir <9957501@gmail.com>
2021-02-28 17:49:54 +0000

    Add more tutorials

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,4 +5,7 @@ document.write(Solution('1.0.4', '1.0.8'));
 
 document.write('<br/>Comparing 1.3 and 1.2.9.9.9.9<br/>');
 document.write(Solution('1.3', '1.2.9.9.9.9'));
+document.write('<br/>Comparing 1.10 and 1.1.2<br/>');
+document.write(Solution('1.10', '1.1.2'));
+document.write('<br/>Feel free to add more lines<br/>');
 console.log('webpack starter running');
